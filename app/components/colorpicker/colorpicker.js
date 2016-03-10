4d71cd8d76ee5761b0b3486b854fb21d168139f5
Elmar Burke <elmar.burke@symetics.com>
2016-03-10 09:41:32 +0100

    controller colorpicker

--- app/components/colorpicker/colorpicker.js
+++ app/components/colorpicker/colorpicker.js
@@ -6,7 +6,10 @@ angular.module('myApp.colorpicker', [])
       r: '@',
       g: '@',
       b: '@',
-      a: '@'
+      a: '='
     },
-    template: '<div style="height: 100px; background-color: rgba({{$ctrl.r}}, {{$ctrl.g}}, {{$ctrl.b}}, {{$ctrl.a}});"></div><input type="range" ng-model="$ctrl.r" max="255"><input type="range" ng-model="$ctrl.g" max="255"><input type="range" ng-model="$ctrl.b" max="255"><input type="range" ng-model="$ctrl.a" max="1" step="0.01">'
+    controller: function () {
+      this.a = this.a || 0.5
+    },
+    templateUrl: 'components/colorpicker/colorpicker.html'
   })
